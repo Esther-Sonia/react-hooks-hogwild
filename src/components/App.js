e8e8243744d68024e2a89b2d70b678788c5eb74b
Esther-Sonia <esthersonia21@gmail.com>
2025-04-14 08:06:02 +0300

    Set up App.js with state management for hog list, filtering, sorting, hiding, and adding new hogs

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,54 @@
 import React from "react";
 import Nav from "./Nav";
+import HogTile from './HogTile'
+import HogFilter from './HogFilter';
+import Sort from './Sort';
 
 import hogs from "../porkers_data";
 
 function App() {
+	const [greasedOnly, setGreasedOnly] = React.useState(false);
+    const [sortType, setSortType] = React.useState("");
+	const [hiddenHogs, setHiddenHogs] = React.useState({});
+	
+    
+	let filteredHogs = greasedOnly
+	? hogs.filter((hog) => hog.greased)
+		: hogs;
+
+	if (sortType === "name") {
+		filteredHogs.sort((a, b) => a.name.localeCompare(b.name));
+	} else if (sortType === "weight") {
+		filteredHogs.sort((a, b) => a.weight - b.weight);
+	}
+
+	const toggleHide = (hogName) => {
+		setHiddenHogs((prevState) => ({
+			...prevState,
+			[hogName]: !prevState[hogName], 
+		}));
+	};
+
+
+
 	return (
 		<div className="App">
 			<Nav />
+			<div style={{ padding: "1rem" }}>
+				<HogFilter greasedHogs={greasedOnly} setGreasedHogs={setGreasedOnly} />
+				<Sort sortBy={sortType} setSortBy={setSortType} />
+		</div>
+		<div className="ui grid container">
+				{filteredHogs.map((hog) => (
+					<HogTile key={hog.name} hog={hog}
+					isHidden={hiddenHogs[hog.name]}
+					toggleHide={toggleHide} 
+					 />
+				))}
+			</div>
 		</div>
 	);
+	
 }
 
 export default App;
